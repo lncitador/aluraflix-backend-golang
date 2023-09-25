b11b15967d529376f60cc1641376c408dbad2007
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 14:18:09 -0300

    feat(http): adiciona endpoint de listagem de categorias

--- internal/delivery/http/routes.go
+++ internal/delivery/http/routes.go
@@ -3,16 +3,19 @@ package http
 import (
 	"fmt"
 	"github.com/lncitador/alura-flix-backend/config"
+	"github.com/lncitador/alura-flix-backend/internal/delivery/http/categorias"
 	"github.com/lncitador/alura-flix-backend/internal/delivery/http/videos"
 )
 
 type Routes struct {
 	*videos.VideoHandlers
+	*categorias.CategoriaHandlers
 }
 
 func InitRoutes(config *config.Config) *Routes {
 	return &Routes{
-		VideoHandlers: videos.NewVideoHandlers(config),
+		VideoHandlers:     videos.NewVideoHandlers(config),
+		CategoriaHandlers: categorias.NewCategoriaHandlers(config),
 	}
 }
 
@@ -20,4 +23,5 @@ func (r *Routes) Register() {
 	fmt.Print("Registering routes...\n")
 
 	r.VideoHandlers.Register()
+	r.CategoriaHandlers.Register()
 }
