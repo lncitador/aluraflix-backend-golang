13030c5d5d30f0da32ba4ebaaf3a7648a57098c8
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 20:38:05 -0300

    feat(video): adiciona filtro de busca na listagem de vídeos
    - Adiciona a importação do pacote domain
    - Cria a variável query do tipo VideoQuery
    - Obtém o parâmetro de busca da requisição e define na query
    - Chama o método FindAll do useCase passando a query como argumento
    - Trata o erro retornando uma resposta JSON com o erro em caso de falha

--- internal/delivery/http/videos/index.go
+++ internal/delivery/http/videos/index.go
@@ -2,10 +2,16 @@ package videos
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lncitador/alura-flix-backend/internal/domain"
 )
 
 func (h VideoHandlers) index(c *gin.Context) {
-	videos, err := h.useCase.FindAll()
+	var query domain.VideoQuery
+	search := c.Query("search")
+	query.SetSearch(search)
+
+	videos, err := h.useCase.FindAll(query)
+
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
