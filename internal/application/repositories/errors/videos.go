1d0f201be443a2b49142b4c615b50ebdf452fe35
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 13:57:31 -0300

    feat(repositories): adiciona novos erros em vídeos
    Adiciona os seguintes erros em vídeos:
    - ErrFindByIdVideo: não foi possível encontrar o vídeo
    - ErrVideoIdIsNull: o id do vídeo não pode ser nulo
    - ErrVideoAlreadyExists: o vídeo já existe
    - ErrUpdateVideo: não foi possível atualizar o vídeo
    - ErrDeleteVideo: não foi possível deletar o vídeo

--- internal/application/repositories/errors/videos.go
+++ internal/application/repositories/errors/videos.go
@@ -1,7 +1,9 @@
 package errors
 
 const (
-	ErrFindByIdVideo = "não foi possível encontrar o vídeo"
-	ErrUpdateVideo   = "não foi possível atualizar o vídeo"
-	ErrDeleteVideo   = "não foi possível deletar o vídeo"
+	ErrFindByIdVideo      = "não foi possível encontrar o vídeo"
+	ErrVideoIdIsNull      = "o id do vídeo não pode ser nulo"
+	ErrVideoAlreadyExists = "o vídeo já existe"
+	ErrUpdateVideo        = "não foi possível atualizar o vídeo"
+	ErrDeleteVideo        = "não foi possível deletar o vídeo"
 )
