ce75b53c733be4ca293cae0f20fc07061bef6db4
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 00:56:32 -0300

    feat(repositories): adiciona mensagens de erro para findById, update e delete de vídeos

--- internal/application/repositories/errors/videos.go
+++ internal/application/repositories/errors/videos.go
@@ -2,4 +2,6 @@ package errors
 
 const (
 	ErrFindByIdVideo = "não foi possível encontrar o vídeo"
+	ErrUpdateVideo   = "não foi possível atualizar o vídeo"
+	ErrDeleteVideo   = "não foi possível deletar o vídeo"
 )
