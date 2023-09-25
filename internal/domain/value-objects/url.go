bbf7cd5108eefd71daa41feec73b9c952f32d4d2
Walaff Fernandes <lncitador@icloud.com>
2023-09-24 21:44:03 -0300

    feat(url): adiciona o arquivo url.go ao pacote value_objects
    O arquivo url.go foi adicionado ao pacote value_objects. Ele contém a implementação do tipo URL, que representa uma URL válida. A URL é validada com base em uma expressão regular e possui métodos para obter o valor da URL e convertê-la em string. Além disso, o tipo URL implementa as interfaces Scanner e Valuer para suportar a leitura e gravação em um banco de dados.

---
+++ internal/domain/value-objects/url.go
@@ -0,0 +1,60 @@
+package value_objects
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"github.com/lncitador/alura-flix-backend/internal/domain/value-objects/errors"
+	"regexp"
+)
+
+type URL ValueObject[string]
+
+func NewURL(value string) (*URL, error) {
+	rgx := `^(http|https):\/\/[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}`
+
+	if len(value) == 0 {
+		return nil, fmt.Errorf(errors.URL_VAZIO)
+	}
+
+	match, err := regexp.MatchString(rgx, value)
+	if err != nil {
+		return nil, err
+	}
+
+	if !match {
+		return nil, fmt.Errorf(errors.URL_INVALIDO)
+	}
+
+	return &URL{value: value}, nil
+}
+
+func (u *URL) GetValue() string {
+	return u.value
+}
+
+func (u *URL) ToString() string {
+	return u.value
+}
+
+// Scan implements the Scanner interface.
+func (u *URL) Scan(value interface{}) error {
+	if value == nil {
+		return fmt.Errorf(errors.VALOR_NULO)
+	}
+
+	switch v := value.(type) {
+	case []byte:
+		u.value = string(v)
+	case string:
+		u.value = v
+	default:
+		return fmt.Errorf(errors.TIPO_INVALIDO)
+	}
+
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (u *URL) Value() (driver.Value, error) {
+	return u.value, nil
+}
