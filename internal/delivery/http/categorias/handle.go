c9563729d54e60f99a7879e8c8d423e2f1eb6c72
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 14:17:05 -0300

    feat(categorias): adiciona arquivo handle.go para categoria

---
+++ internal/delivery/http/categorias/handle.go
@@ -0,0 +1,34 @@
+package categorias
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lncitador/alura-flix-backend/config"
+	"github.com/lncitador/alura-flix-backend/internal/application/repositories/impl"
+	"github.com/lncitador/alura-flix-backend/internal/application/usecases"
+)
+
+type CategoriaHandlers struct {
+	router  *gin.RouterGroup
+	useCase *usecases.CategoriasUseCase
+}
+
+func NewCategoriaHandlers(config *config.Config) *CategoriaHandlers {
+	db := config.GetDb()
+
+	repo := impl.NewCategoriaRepository(db)
+	useCase := usecases.NewCategoriasUseCase(repo)
+
+	v1 := config.GetRouter().Group("/api/v1")
+
+	return &CategoriaHandlers{
+		router:  v1,
+		useCase: useCase,
+	}
+}
+
+func (h CategoriaHandlers) Register() {
+	categorias := h.router.Group("/categorias")
+	{
+		categorias.GET("/", h.index)
+	}
+}
