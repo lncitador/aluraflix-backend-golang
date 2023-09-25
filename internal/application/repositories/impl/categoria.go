230ca50ba9bfcfba1cc4aaebb9ff8a9a82e576f4
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 14:04:20 -0300

    feat(repositories): implementação do repositório de Categoria
    Implementa o repositório de Categoria com os métodos FindAll, FindById, Create, Update e Delete. Esses métodos permitem buscar todas as categorias, buscar uma categoria pelo ID, criar uma nova categoria, atualizar uma categoria existente e excluir uma categoria.

---
+++ internal/application/repositories/impl/categoria.go
@@ -0,0 +1,62 @@
+package impl
+
+import (
+	"errors"
+	e "github.com/lncitador/alura-flix-backend/internal/application/repositories/errors"
+	"github.com/lncitador/alura-flix-backend/internal/domain"
+	vo "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
+	"gorm.io/gorm"
+)
+
+type CategoriaRepository struct {
+	db *gorm.DB
+}
+
+func NewCategoriaRepository(db *gorm.DB) *CategoriaRepository {
+	return &CategoriaRepository{db}
+}
+
+func (r CategoriaRepository) FindAll() ([]domain.Categoria, error) {
+	var categorias []domain.Categoria
+	if err := r.db.Find(&categorias).Error; err != nil {
+		return nil, err
+	}
+
+	return categorias, nil
+}
+
+func (r CategoriaRepository) FindById(id *vo.UniqueEntityID) (*domain.Categoria, error) {
+	if id == nil {
+		return nil, errors.New(e.ErrCategoriaIdIsNull)
+	}
+	var categoria domain.Categoria
+	if err := r.db.First(&categoria, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &categoria, nil
+}
+
+func (r CategoriaRepository) Create(data domain.Categoria) error {
+	if err := r.db.Create(&data).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r CategoriaRepository) Update(data domain.Categoria) error {
+	if err := r.db.Save(&data).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r CategoriaRepository) Delete(id *vo.UniqueEntityID) error {
+	if err := r.db.Delete(&domain.Categoria{}, id).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
