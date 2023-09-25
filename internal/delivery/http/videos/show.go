73e7dd00175a8326327bd7136f9f090e10157726
Walaff Fernandes <lncitador@icloud.com>
2023-09-24 22:50:15 -0300

    feat(http): Adiciona função show para exibir um vídeo pelo ID

---
+++ internal/delivery/http/videos/show.go
@@ -0,0 +1,28 @@
+package videos
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lncitador/alura-flix-backend/internal/application/repositories/errors"
+	v "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
+)
+
+func (h VideoHandlers) show(c *gin.Context) {
+	id := c.Param("id")
+	uid, err := v.NewUniqueEntityID(&id)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	video, err := h.useCase.FindById(uid)
+	if err != nil {
+		if err.Error() != errors.ErrFindByIdVideo {
+			c.JSON(404, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, video)
+}
