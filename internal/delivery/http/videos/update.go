4bdd7bd1d6fadf5c352e72dd745b2dfa0f3f43d5
Walaff Fernandes <lncitador@icloud.com>
2023-09-24 22:50:33 -0300

    feat(http): adiciona handle de atualização de vídeo
    Adiciona o arquivo `update.go` no pacote `videos` do diretório `internal/delivery/http`. O arquivo contém a implementação da função `update` que é responsável por atualizar um vídeo no sistema. O endpoint recebe o ID do vídeo como parâmetro e uma requisição JSON contendo os dados atualizados do vídeo. A função faz a validação do ID, verifica se os dados da requisição estão corretos e chama o caso de uso responsável por atualizar o vídeo. Em seguida, retorna a resposta JSON contendo o vídeo atualizado.
    
    Closes #123

---
+++ internal/delivery/http/videos/update.go
@@ -0,0 +1,30 @@
+package videos
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lncitador/alura-flix-backend/internal/domain"
+	v "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
+)
+
+func (h VideoHandlers) update(c *gin.Context) {
+	id := c.Param("id")
+	uid, err := v.NewUniqueEntityID(&id)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	var dto domain.VideoInput
+	if err := c.ShouldBindJSON(&dto); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	video, err := h.useCase.Update(uid, dto)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, video)
+}
