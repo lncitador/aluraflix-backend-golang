d3189fe5e55eccfa4ef74baf582b2bfa94186385
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 13:57:15 -0300

    feat(repositories): adiciona arquivo de erros de categorias
    Adiciona o arquivo `categorias.go` no pacote `errors`, contendo as constantes de erro relacionadas às categorias:
    
    - ErrFindByIdCategoria: não foi possível encontrar a categoria
    - ErrCategoriaNotFound: categoria não encontrada
    - ErrCategoriaIdIsNull: o id da categoria não pode ser nulo
    - ErrUpdateCategoria: não foi possível atualizar a categoria
    - ErrDeleteCategoria: não foi possível deletar a categoria
    - ErrCategoriaAlreadyExists: categoria já existe

---
+++ internal/application/repositories/errors/categorias.go
@@ -0,0 +1,10 @@
+package errors
+
+const (
+	ErrFindByIdCategoria      = "não foi possível encontrar a categoria"
+	ErrCategoriaNotFound      = "categoria não encontrada"
+	ErrCategoriaIdIsNull      = "o id da categoria não pode ser nulo"
+	ErrUpdateCategoria        = "não foi possível atualizar a categoria"
+	ErrDeleteCategoria        = "não foi possível deletar a categoria"
+	ErrCategoriaAlreadyExists = "categoria já existe"
+)
