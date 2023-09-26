e78d6581715225bbe088444bdb9e98b58c2f0980
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 23:08:39 -0300

    feat(video-query): adiciona método SetLimit e Limit na struct VideoQuery
    Adiciona o método SetLimit para definir o limite de busca dos vídeos e o método Limit para obter o limite atual. O limite deve ser um número entre 10 e 100. Caso o limite seja menor que 10 ou maior que 100, é retornado um erro.

--- internal/domain/video-query.go
+++ internal/domain/video-query.go
@@ -9,6 +9,7 @@ import (
 type VideoQuery struct {
 	search *string
 	page   *int
+	limit  *int
 }
 
 func (q *VideoQuery) SetSearch(value string) {
@@ -41,4 +42,31 @@ func (q *VideoQuery) SetPage(value string) error {
 
 func (q *VideoQuery) Page() *int {
 	return q.page
+}
+
+func (q *VideoQuery) SetLimit(value string) error {
+	if value != "" {
+		if limit, err := strconv.Atoi(value); err != nil {
+			return fmt.Errorf("limit must be a number")
+		} else if limit < 10 {
+			return fmt.Errorf("limit must be greater than 10")
+		} else if limit > 100 {
+			return fmt.Errorf("limit must be less than 100")
+		} else {
+			q.limit = &limit
+
+			return nil
+		}
+	}
+
+	return nil
+}
+
+func (q *VideoQuery) Limit() *int {
+	if q.limit == nil {
+		limit := 10
+		q.limit = &limit
+	}
+
+	return q.limit
 }
