c22d42bc707647a3176a1ac291e159c37d72ee5c
Walaff Fernandes <lncitador@icloud.com>
2023-09-26 13:11:10 -0300

    feat(pagination): adiciona estrutura de paginação
    Adiciona a estrutura de paginação para manipulação de dados paginados.
    A estrutura Pagination[Model] possui os seguintes atributos: Data, FirstPage, LastPage, PrevPage, NextPage e TotalPage. Além disso, foi definida a interface PaginationContract para definir os métodos Page(), Limit() e Total().
    
    A função Paginate() foi implementada para realizar a paginação dos dados de acordo com a configuração definida. Ela recebe a URL, os dados e a configuração de paginação como parâmetros. A função calcula o número total de páginas com base no limite e no total de registros. Em seguida, verifica se há páginas anteriores e posteriores, e define as URLs correspondentes. Também define a URL da primeira página e da última

---
+++ internal/domain/pagination.go
@@ -0,0 +1,89 @@
+package domain
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+type Pagination[Model any] struct {
+	Data      []Model `json:"data,omitempty"`
+	FirstPage string  `json:"firstPage,omitempty"`
+	LastPage  *string `json:"lastPage,omitempty"`
+	PrevPage  *string `json:"prevPage,omitempty"`
+	NextPage  string  `json:"nextPage,omitempty"`
+	TotalPage int64   `json:"totalPage,omitempty"`
+}
+
+type PaginationContract interface {
+	Page() *int
+	Limit() *int
+	Total() *int64
+}
+
+func (p *Pagination[Model]) Paginate(URL string, data *[]Model, config PaginationContract) error {
+	if config.Page() != nil {
+		limit := *config.Limit()
+		total := *config.Total()
+
+		p.TotalPage = total / int64(limit)
+
+		if total%int64(limit) > 0 {
+			p.TotalPage++
+		}
+
+		if *config.Page() > 1 {
+			prevPage, err := url.Parse(URL)
+			if err != nil {
+				return fmt.Errorf("error to parse url")
+			}
+
+			query := prevPage.Query()
+			query.Set("page", fmt.Sprintf("%d", *config.Page()-1))
+
+			p.PrevPage = prepare(prevPage, query)
+		}
+
+		if *config.Page() < int(p.TotalPage) {
+			nextPage, _ := url.Parse(URL)
+			query := nextPage.Query()
+			query.Set("page", fmt.Sprintf("%d", *config.Page()+1))
+
+			p.NextPage = *prepare(nextPage, query)
+		}
+
+		{
+			firstPage, _ := url.Parse(URL)
+			query := firstPage.Query()
+			query.Set("page", "1")
+
+			p.FirstPage = *prepare(firstPage, query)
+		}
+
+		{
+			lastPage, _ := url.Parse(URL)
+			query := lastPage.Query()
+			query.Set("page", fmt.Sprintf("%d", p.TotalPage))
+
+			p.LastPage = prepare(lastPage, query)
+		}
+
+		p.Data = *data
+
+		return nil
+	}
+
+	return fmt.Errorf("page is required")
+}
+
+func prepare(url *url.URL, query url.Values) *string {
+	if strings.HasSuffix(url.Path, "/") {
+		path := url.Path[:len(url.Path)-1]
+		url.Path = path
+	}
+
+	url.RawQuery = query.Encode()
+	path := url.String()
+
+	return &path
+}
