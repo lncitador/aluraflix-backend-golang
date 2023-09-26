ecc0fdabf26ae380e13c01de480c8a0aba2a0680
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 22:38:35 -0300

    feat(video-query): adiciona método SetPage e Page na struct VideoQuery
    Adiciona o método SetPage para definir o número da página na busca de vídeos e o método Page para retornar o número da página.

--- internal/domain/video-query.go
+++ internal/domain/video-query.go
@@ -2,11 +2,13 @@ package domain
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 type VideoQuery struct {
 	search *string
+	page   *int
 }
 
 func (q *VideoQuery) SetSearch(value string) {
@@ -19,4 +21,32 @@ func (q *VideoQuery) SetSearch(value string) {
 
 func (q *VideoQuery) Search() *string {
 	return q.search
+}
+
+func (q *VideoQuery) SetPage(value string) error {
+	page := 1
+	if value != "" {
+		if page, err := strconv.Atoi(value); err != nil {
+			return fmt.Errorf("page must be a number")
+		} else if page < 1 {
+			return fmt.Errorf("page must be greater than 0")
+		} else {
+			q.page = &page
+
+			return nil
+		}
+	}
+
+	q.page = &page
+
+	return nil
+}
+
+func (q *VideoQuery) Page() *int {
+	if q.page == nil {
+		page := 1
+		q.page = &page
+	}
+
+	return q.page
 }
