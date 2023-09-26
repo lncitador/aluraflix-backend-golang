db5b8b1f6e92d0842eb9a79433977e680c78276c
Walaff Fernandes <lncitador@icloud.com>
2023-09-25 22:39:12 -0300

    test(video-query): adiciona testes unitários para a struct VideoQuery
    Adiciona testes unitários para os métodos `Page` e `SetPage` da classe
    `VideoQuery` no arquivo `internal/domain/video-query_test.go`. Os testes
    verificam se os métodos retornam os valores corretos e se lançam erros em casos
    específicos.

--- internal/domain/video-query_test.go
+++ internal/domain/video-query_test.go
@@ -26,3 +26,39 @@ func TestVideoQuery_SetSearch(t *testing.T) {
 		assert.Equal(t, "%search%", *q.search)
 	})
 }
+
+func TestVideoQuery_Page(t *testing.T) {
+	t.Run("should return 1 if page is nil", func(t *testing.T) {
+		q := &VideoQuery{}
+		assert.Equal(t, 1, *q.Page())
+	})
+
+	t.Run("should return page value from query", func(t *testing.T) {
+		value := 2
+		q := &VideoQuery{page: &value}
+		assert.Equal(t, &value, q.Page())
+	})
+}
+
+func TestVideoQuery_SetPage(t *testing.T) {
+	t.Run("should set page value from query", func(t *testing.T) {
+		q := &VideoQuery{}
+		_ = q.SetPage("2")
+
+		assert.Equal(t, 2, *q.page)
+	})
+
+	t.Run("should return error if page is not a number", func(t *testing.T) {
+		q := &VideoQuery{}
+		err := q.SetPage("a")
+
+		assert.EqualError(t, err, "page must be a number")
+	})
+
+	t.Run("should return error if page is less than 1", func(t *testing.T) {
+		q := &VideoQuery{}
+		err := q.SetPage("0")
+
+		assert.EqualError(t, err, "page must be greater than 0")
+	})
+}
