45d926df75ce4a20b99453412e7083b1bced9f64
Walaff Fernandes <lncitador@icloud.com>
2023-09-27 14:55:34 -0300

    feat(http): adiciona tratamento de erros ao atualizar vídeo

--- internal/delivery/http/videos/update.go
+++ internal/delivery/http/videos/update.go
@@ -4,13 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/lncitador/alura-flix-backend/internal/domain"
 	v "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
+	"github.com/lncitador/alura-flix-backend/pkg/validations"
+	"net/http"
 )
 
 func (h VideoHandlers) update(c *gin.Context) {
 	id := c.Param("id")
 	uid, err := v.NewUniqueEntityID(&id)
 	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
 		return
 	}
 
@@ -21,8 +23,11 @@ func (h VideoHandlers) update(c *gin.Context) {
 	}
 
 	video, err := h.useCase.Update(uid, dto)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
+	if notFound, validation := validations.GetErrorsByValidation(err); validation != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": validation})
+		return
+	} else if notFound != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 		return
 	}
 
