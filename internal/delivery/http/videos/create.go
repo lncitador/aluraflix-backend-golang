de284bc1d49d077de2e890e002d90e18210a5267
Walaff Fernandes <lncitador@icloud.com>
2023-09-27 14:05:34 -0300

    feat(http): adiciona validação de dados no endpoint de criação de vídeos

--- internal/delivery/http/videos/create.go
+++ internal/delivery/http/videos/create.go
@@ -3,6 +3,8 @@ package videos
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/lncitador/alura-flix-backend/internal/domain"
+	"github.com/lncitador/alura-flix-backend/pkg/validations"
+	"net/http"
 )
 
 func (h VideoHandlers) create(c *gin.Context) {
@@ -13,8 +15,11 @@ func (h VideoHandlers) create(c *gin.Context) {
 	}
 
 	video, err := h.useCase.Create(dto)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
+	if internal, validation := validations.GetErrorsByValidation(err); validation != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": validation})
+		return
+	} else if internal != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
