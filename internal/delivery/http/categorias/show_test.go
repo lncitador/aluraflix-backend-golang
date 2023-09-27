473755b886c9aa132d50488e92c336f60f923c34
Walaff Fernandes <lncitador@icloud.com>
2023-09-27 13:42:44 -0300

    feat(categorias): Adiciona teste para o handler de exibição de categoria
    Adiciona o arquivo de teste `show_test.go` no pacote `categorias` para testar o handler responsável por exibir uma categoria específica.
    O teste verifica se é retornada uma categoria com um ID válido e se é retornado um status de erro quando o ID é inválido.

---
+++ internal/delivery/http/categorias/show_test.go
@@ -0,0 +1,33 @@
+package categorias
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCategoriaHandlers_show(t *testing.T) {
+	sut := setupSut()
+	id := sut.ID
+	url := sut.URL + "/"
+
+	sut.r.GET(url+":id", sut.show)
+
+	t.Run("should return a categoria", func(t *testing.T) {
+
+		req, _ := http.NewRequest("GET", url+id, nil)
+		w := httptest.NewRecorder()
+		sut.r.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
+
+	t.Run("should not return a categoria with invalid id", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", url+"invalid", nil)
+		w := httptest.NewRecorder()
+		sut.r.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+}
