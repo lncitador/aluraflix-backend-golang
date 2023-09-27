8a5e78613dde56b562590720f4345cd8161edd5a
Walaff Fernandes <lncitador@icloud.com>
2023-09-27 13:49:59 -0300

    refactor(videos): rota para manipulação de vídeos
    - Adiciona rota para listar todos os vídeos
    - Adiciona rota para exibir um vídeo pelo ID
    - Adiciona rota para criar um novo vídeo
    - Adiciona rota para atualizar um vídeo existente
    - Adiciona rota para deletar um vídeo pelo ID
    
    A rota `/v1/videos` foi adicionada para manipulação de vídeos no endpoint da API. Essa rota possui as seguintes funcionalidades:
    
    - `GET /v1/videos`: Lista todos os vídeos cadastrados
    - `GET /v1/videos/:id`: Exibe os detalhes de um vídeo específico pelo seu ID
    - `POST /v1/videos`: Cria um novo vídeo com os dados fornecidos
    - `PUT /v1/videos/:id`: Atualiza os dados de um vídeo existente pelo seu ID
    - `DELETE /v1/videos/:id´: Remove um video específico pelo seu ID

--- internal/delivery/http/videos/handle.go
+++ internal/delivery/http/videos/handle.go
@@ -2,36 +2,30 @@ package videos
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/lncitador/alura-flix-backend/config"
-	"github.com/lncitador/alura-flix-backend/internal/application/repositories/impl"
 	"github.com/lncitador/alura-flix-backend/internal/application/usecases"
 )
 
 type VideoHandlers struct {
-	router  *gin.Engine
+	router  *gin.RouterGroup
 	useCase *usecases.VideosUseCase
 }
 
-func NewVideoHandlers(config *config.Config) *VideoHandlers {
-	db := config.GetDb()
-
-	repo := impl.NewVideoRepository(db)
+func NewVideoHandlers(router *gin.RouterGroup, repo usecases.VideoRepositoryContract) *VideoHandlers {
 	useCase := usecases.NewVideosUseCase(repo)
 
 	return &VideoHandlers{
-		router:  config.GetRouter(),
+		router:  router,
 		useCase: useCase,
 	}
 }
 
 func (h VideoHandlers) Register() {
-	v1 := h.router.Group("/api/v1")
-	videos := v1.Group("/videos")
+	videosV1 := h.router.Group("/v1/videos")
 	{
-		videos.GET("/", h.index)
-		videos.GET("/:id", h.show)
-		videos.POST("/", h.create)
-		videos.PUT("/:id", h.update)
-		videos.DELETE("/:id", h.delete)
+		videosV1.GET("/", h.index)
+		videosV1.GET("/:id", h.show)
+		videosV1.POST("/", h.create)
+		videosV1.PUT("/:id", h.update)
+		videosV1.DELETE("/:id", h.delete)
 	}
 }
