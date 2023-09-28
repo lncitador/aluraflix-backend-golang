e143e10ee4167e1e69da61b80c0b61395557a4d1
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 13:49:35 -0300

    feat(http-routes): adiciona handler de autorização
    Adiciona o pacote "github.com/lncitador/alura-flix-backend/internal/delivery/http/authorization" para implementar o handler de autorização na rota HTTP.
    (issue #master)

--- internal/delivery/http/routes.go
+++ internal/delivery/http/routes.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/lncitador/alura-flix-backend/config"
 	"github.com/lncitador/alura-flix-backend/internal/application/repositories/impl"
+	"github.com/lncitador/alura-flix-backend/internal/delivery/http/authorization"
 	"github.com/lncitador/alura-flix-backend/internal/delivery/http/categorias"
 	"github.com/lncitador/alura-flix-backend/internal/delivery/http/videos"
 )
@@ -11,6 +12,7 @@ import (
 type Routes struct {
 	*videos.VideoHandlers
 	*categorias.CategoriaHandlers
+	*authorization.AuthHandlers
 }
 
 func InitRoutes(config *config.Config) *Routes {
@@ -20,16 +22,19 @@ func InitRoutes(config *config.Config) *Routes {
 	db := config.GetDb()
 	videoRepository := impl.NewVideoRepository(db)
 	categoriasRepository := impl.NewCategoriaRepository(db)
+	usuariosRepository := impl.NewUsuarioRepository(db)
 
 	return &Routes{
 		VideoHandlers:     videos.NewVideoHandlers(api, videoRepository),
 		CategoriaHandlers: categorias.NewCategoriaHandlers(api, categoriasRepository),
+		AuthHandlers:      authorization.NewAuthHandlers(api, usuariosRepository),
 	}
 }
 
 func (r *Routes) Register() {
 	fmt.Print("Registering routes...\n")
 
-	r.VideoHandlers.Register()
-	r.CategoriaHandlers.Register()
+	r.VideoHandlers.Register(r.AuthMiddleware)
+	r.CategoriaHandlers.Register(r.AuthMiddleware)
+	r.AuthHandlers.Register()
 }
