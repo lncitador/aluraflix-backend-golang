c1c037b06457ffea89dbc372e53cc4fc3f3149dd
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 13:43:36 -0300

    fix(domain): remove criptografia de senha no usuário-input

--- internal/domain/usuario-input.go
+++ internal/domain/usuario-input.go
@@ -1,7 +1,6 @@
 package domain
 
 import (
-	"golang.org/x/crypto/bcrypt"
 	"strings"
 )
 
@@ -25,16 +24,6 @@ func (u *UsuarioInput) validate() error {
 	u.prepare()
 	if err := validate.Struct(u); err != nil {
 		return err
-	}
-
-	if u.Password != nil {
-		hash, err := bcrypt.GenerateFromPassword([]byte(*u.Password), bcrypt.DefaultCost)
-
-		if err != nil {
-			return err
-		}
-
-		*u.Password = string(hash)
 	}
 
 	return nil
