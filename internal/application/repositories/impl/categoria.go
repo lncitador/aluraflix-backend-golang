66b83e06cc9e230d5728ad934d608f67a3c995c0
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 20:59:07 -0300

    fix(repositories): corrige validações e erros no pacote de domínio de categoria

--- internal/application/repositories/impl/categoria.go
+++ internal/application/repositories/impl/categoria.go
@@ -5,7 +5,9 @@ import (
 	e "github.com/lncitador/alura-flix-backend/internal/application/repositories/errors"
 	"github.com/lncitador/alura-flix-backend/internal/domain"
 	vo "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
+	. "github.com/lncitador/alura-flix-backend/pkg/errors"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 type CategoriaRepository struct {
@@ -16,50 +18,66 @@ func NewCategoriaRepository(db *gorm.DB) *CategoriaRepository {
 	return &CategoriaRepository{db}
 }
 
-func (r CategoriaRepository) FindAll(query *domain.CategoriaQuery) ([]domain.Categoria, error) {
+func (r CategoriaRepository) FindAll(query *domain.CategoriaQuery) ([]domain.Categoria, *Error) {
 	if usuarioID := query.UsuarioID(); usuarioID != nil {
 		r.db = r.db.Where("usuario_id = ?", usuarioID)
 	}
 
 	var categorias []domain.Categoria
 	if err := r.db.Find(&categorias).Error; err != nil {
-		return nil, err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, NewErrorByNotFound(err)
+		}
+
+		return nil, NewErrorByBadRequest(err)
 	}
 
 	return categorias, nil
 }
 
-func (r CategoriaRepository) FindById(id *vo.UniqueEntityID) (*domain.Categoria, error) {
+func (r CategoriaRepository) FindById(id *vo.UniqueEntityID) (*domain.Categoria, *Error) {
 	if id == nil {
-		return nil, errors.New(e.ErrCategoriaIdIsNull)
+		return nil, NewError(http.StatusBadRequest, e.ErrCategoriaIdIsNull, "")
 	}
 	var categoria domain.Categoria
 	if err := r.db.First(&categoria, id).Error; err != nil {
-		return nil, err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, NewErrorByNotFound(err)
+		}
+
+		return nil, NewErrorByBadRequest(err)
 	}
 
 	return &categoria, nil
 }
 
-func (r CategoriaRepository) Create(data domain.Categoria) error {
+func (r CategoriaRepository) Create(data domain.Categoria) *Error {
 	if err := r.db.Create(&data).Error; err != nil {
-		return err
+		return NewErrorByBadRequest(err)
 	}
 
 	return nil
 }
 
-func (r CategoriaRepository) Update(data domain.Categoria) error {
+func (r CategoriaRepository) Update(data domain.Categoria) *Error {
 	if err := r.db.Save(&data).Error; err != nil {
-		return err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return NewErrorByNotFound(err)
+		}
+
+		return NewErrorByBadRequest(err)
 	}
 
 	return nil
 }
 
-func (r CategoriaRepository) Delete(id *vo.UniqueEntityID) error {
+func (r CategoriaRepository) Delete(id *vo.UniqueEntityID) *Error {
 	if err := r.db.Delete(&domain.Categoria{}, id).Error; err != nil {
-		return err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return NewErrorByNotFound(err)
+		}
+
+		return NewErrorByBadRequest(err)
 	}
 
 	return nil
