a486bea6ec2d5f8a6f746181348f4deceb081bf9
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 19:46:48 -0300

    feat(categorias): adiciona verificação de autorização no endpoint de exibição

--- internal/delivery/http/categorias/show.go
+++ internal/delivery/http/categorias/show.go
@@ -2,21 +2,35 @@ package categorias
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lncitador/alura-flix-backend/internal/domain"
 	v "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
+	"net/http"
 )
 
 func (h CategoriaHandlers) show(c *gin.Context) {
 	id := c.Param("id")
 	uid, err := v.NewUniqueEntityID(&id)
-
 	if err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
+	user, exists := c.Get("user")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	userId := user.(*domain.UsuarioDto).ID
+
 	categoria, err := h.useCase.FindById(uid)
 	if err != nil {
 		c.JSON(404, gin.H{"error": err.Error()})
+		return
+	}
+
+	if categoria.UsuarioID != userId {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
 		return
 	}
 
