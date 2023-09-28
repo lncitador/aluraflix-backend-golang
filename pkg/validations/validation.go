7e9fbdd0217906bdc7bd8e24957a70ee11eabdb8
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 00:33:36 -0300

    feat(validations): Adiciona função ErrosAsValidationByField
    Adiciona a função ErrosAsValidationByField no arquivo validation.go que retorna um booleano indicando se o erro é um erro de validação e a mensagem de erro associada. Essa função recebe um erro e uma string para indicar qual campo deve ser validado. Se o erro for do tipo validator.ValidationErrors, a função percorre os campos de erro e retorna o booleano e a mensagem de erro correspondente ao campo indicado. Se o erro for do tipo validator.FieldError, a função retorna o booleano e a mensagem de erro. Caso contrário, a função retorna nil e uma string vazia.
    
    Essa função substitui a função GetErrorsByValidation que está marcada como obsoleta.

--- pkg/validations/validation.go
+++ pkg/validations/validation.go
@@ -5,6 +5,30 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+// ErrosAsValidationByField returns a boolean indicating if the error is a validation error and the error message
+func ErrosAsValidationByField(err error, by string) (*bool, string) {
+	errValidation := true
+	if err != nil {
+		var fields validator.ValidationErrors
+		var field validator.FieldError
+		if errors.As(err, &fields) {
+			for _, field := range fields {
+				if field.Field() == by {
+					return &errValidation, field.Error()
+				}
+			}
+		} else if errors.As(err, &field) {
+			return &errValidation, field.Error()
+		}
+
+		return new(bool), ""
+	}
+
+	return nil, ""
+}
+
+// GetErrorsByValidation returns a boolean indicating if the error is a validation error and the error message
+// Deprecated: Use ErrosAsValidationByField instead
 func GetErrorsByValidation(err error) (*bool, []string) {
 	if err != nil {
 		var fields validator.ValidationErrors
@@ -16,7 +40,7 @@ func GetErrorsByValidation(err error) (*bool, []string) {
 			}
 			return nil, errs
 		} else if errors.As(err, &field) {
-			return new(bool), []string{field.Error()}
+			return nil, []string{field.Error()}
 		}
 
 		return new(bool), nil
