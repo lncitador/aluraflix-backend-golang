2621ade7021b40e2fa9b37c1213680a16df1fe5a
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 14:21:45 -0300

    feat(authorization): implementar middleware de autenticação
    Implementa o middleware de autenticação para verificar se o token de autorização foi fornecido. Se não for fornecido, retorna um erro 401. Caso contrário, o token é verificado e validado. Se o token for inválido ou expirado, retorna um erro 401. Se o token for válido, o ID do usuário é extraído do token e usado para pesquisar o usuário no banco de dados. Se o usuário não for encontrado, retorna um erro 401. Caso contrário, o usuário é armazenado no contexto e o fluxo segue.

--- internal/delivery/http/authorization/middleware.go
+++ internal/delivery/http/authorization/middleware.go
@@ -2,10 +2,78 @@ package authorization
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	vo "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
 )
 
 func (h AuthHandlers) AuthMiddleware(c *gin.Context) {
-	fmt.Println("AuthMiddleware")
+	tokenStr, err := c.Cookie("Authorization")
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization not provided"})
+		return
+	}
+
+	token, err := parseJWT(tokenStr)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		return
+	}
+
+	expiration, ok := claims["exp"].(float64)
+	if !ok || expiration < float64(time.Now().Unix()) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Token expired"})
+		return
+	}
+
+	sub, ok := claims["sub"].(string)
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Sub claim missing"})
+		return
+	}
+
+	id, err := vo.NewUniqueEntityID(&sub)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	usuario, err := h.useCase.FindById(id)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Set("usuario", usuario)
 	c.Next()
+}
+
+func parseJWT(tokenStr string) (*jwt.Token, error) {
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		secret = "secret"
+	}
+
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
 }
