b5fc27beb76f2c83ede698ead0ee35769ac482fd
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 13:56:08 -0300

    feat(usuarios): implementar métodos de busca, criação, atualização e exclusão de usuários
    Implementa os métodos de busca, criação, atualização e exclusão de usuários no repositório de usuários.
    
    - Implementa o método FindAll para buscar todos os usuários.
    - Implementa o método FindById para buscar um usuário pelo ID.
    - Implementa o método Create para criar um novo usuário.
    - Implementa o método Update para atualizar os dados de um usuário.
    - Implementa o método Delete para excluir um usuário pelo ID.
    - Implementa o método FindByEmail para buscar um usuário pelo email.

--- internal/application/repositories/impl/usuarios.go
+++ internal/application/repositories/impl/usuarios.go
@@ -1,6 +1,8 @@
 package impl
 
 import (
+	"errors"
+	e "github.com/lncitador/alura-flix-backend/internal/application/repositories/errors"
 	"github.com/lncitador/alura-flix-backend/internal/domain"
 	vo "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
 	"gorm.io/gorm"
@@ -17,31 +19,74 @@ func NewUsuarioRepository(db *gorm.DB) *UsuarioRepository {
 }
 
 func (u UsuarioRepository) FindAll(query *struct{}) ([]domain.Usuario, error) {
-	//TODO implement me
-	panic("implement me")
+	var usuarios []domain.Usuario
+
+	if err := u.db.Find(&usuarios).Error; err != nil {
+		return nil, err
+	}
+
+	return usuarios, nil
 }
 
 func (u UsuarioRepository) FindById(id *vo.UniqueEntityID) (*domain.Usuario, error) {
-	//TODO implement me
-	panic("implement me")
+	if id == nil {
+		return nil, errors.New(e.ErrUsuarioIdIsNull)
+	}
+
+	var usuario domain.Usuario
+	if err := u.db.First(&usuario, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errors.New(e.ErrFindByIdUsuario)
+		}
+
+		return nil, err
+	}
+
+	return &usuario, nil
 }
 
 func (u UsuarioRepository) Create(data domain.Usuario) error {
-	//TODO implement me
-	panic("implement me")
+	if err := u.db.Create(&data).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (u UsuarioRepository) Update(data domain.Usuario) error {
-	//TODO implement me
-	panic("implement me")
+	if err := u.db.Save(&data).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (u UsuarioRepository) Delete(id *vo.UniqueEntityID) error {
-	//TODO implement me
-	panic("implement me")
+	if id == nil {
+		return errors.New(e.ErrUsuarioIdIsNull)
+	}
+
+	var usuario domain.Usuario
+	if err := u.db.Delete(&usuario, id).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (u UsuarioRepository) FindByEmail(email *string) (*domain.Usuario, error) {
-	//TODO implement me
-	panic("implement me")
+	if email == nil {
+		return nil, errors.New(e.ErrUsuarioEmailIsNull)
+	}
+
+	var usuario domain.Usuario
+	if err := u.db.Where("email = ?", email).First(&usuario).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errors.New(e.ErrFindByIdUsuario)
+		}
+
+		return nil, err
+	}
+
+	return &usuario, nil
 }
