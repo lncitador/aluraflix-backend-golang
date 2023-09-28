231819891c2d57600ab10a9844f887f95278152a
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 20:58:27 -0300

    fix(repositories): corrige validações e erros no pacote de domínio

--- internal/application/repositories/repository.go
+++ internal/application/repositories/repository.go
@@ -2,12 +2,13 @@ package repositories
 
 import (
 	vo "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
+	. "github.com/lncitador/alura-flix-backend/pkg/errors"
 )
 
 type RepositoryContract[Model any, Query any] interface {
-	FindAll(query Query) ([]Model, error)
-	FindById(id *vo.UniqueEntityID) (*Model, error)
-	Create(data Model) error
-	Update(data Model) error
-	Delete(id *vo.UniqueEntityID) error
+	FindAll(query Query) ([]Model, *Error)
+	FindById(id *vo.UniqueEntityID) (*Model, *Error)
+	Create(data Model) *Error
+	Update(data Model) *Error
+	Delete(id *vo.UniqueEntityID) *Error
 }
