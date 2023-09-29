db8da0ca8410637ffe449eb1e1778304b9767c2f
Walaff Fernandes <lncitador@icloud.com>
2023-09-29 10:29:00 -0300

    feat(http): adiciona verificação de permissão ao buscar vídeo
    Adiciona verificação de permissão ao buscar vídeo. Agora, o endpoint de busca de vídeo verifica se o usuário possui permissão para visualizar o vídeo. Caso contrário, retorna um erro.

--- internal/delivery/http/videos/show.go
+++ internal/delivery/http/videos/show.go
@@ -2,6 +2,7 @@ package videos
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lncitador/alura-flix-backend/internal/domain"
 	v "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
 )
 
@@ -13,7 +14,15 @@ func (h VideoHandlers) show(c *gin.Context) {
 		return
 	}
 
-	video, err := h.useCase.FindById(uid)
+	user, _ := c.Get("user")
+	id = user.(*domain.UsuarioDto).ID
+	userId, err := v.NewUniqueEntityID(&id)
+	if err != nil {
+		c.JSON(err.Status(), gin.H{"error": err})
+		return
+	}
+
+	video, err := h.useCase.FindById(uid, userId)
 	if err != nil {
 		c.JSON(err.Status(), gin.H{"error": err})
 		return
