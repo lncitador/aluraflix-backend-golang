d8b4af3af84cc2fe817386b237b7d3f16a691eb8
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 21:38:26 -0300

    feat(categorias): adiciona validação de usuário ao atualizar categoria

--- internal/delivery/http/categorias/update.go
+++ internal/delivery/http/categorias/update.go
@@ -4,30 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/lncitador/alura-flix-backend/internal/domain"
 	v "github.com/lncitador/alura-flix-backend/internal/domain/value-objects"
-	"github.com/lncitador/alura-flix-backend/pkg/validations"
-	"net/http"
 )
 
 func (h CategoriaHandlers) update(c *gin.Context) {
 	id := c.Param("id")
 	uid, err := v.NewUniqueEntityID(&id)
 	if err != nil {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		c.JSON(err.Status(), gin.H{"error": err})
 		return
 	}
 
+	user, _ := c.Get("user")
 	var dto domain.CategoriaInput
+	dto.UsuarioID = &user.(*domain.UsuarioDto).ID
+
 	if err := c.ShouldBindJSON(&dto); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
 	categoria, err := h.useCase.Update(uid, dto)
-	if notFound, validation := validations.GetErrorsByValidation(err); validation != nil {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": validation})
-		return
-	} else if notFound != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+	if err != nil {
+		c.JSON(err.Status(), gin.H{"error": err})
 		return
 	}
 
