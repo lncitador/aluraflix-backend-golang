89a7ee2f12b43a62fdeed409233a9db412929073
Walaff Fernandes <lncitador@icloud.com>
2023-09-28 21:00:26 -0300

    refactor: corrigir mensagens de erro de usuário
    - Corrigidas as mensagens de erro relacionadas a usuários para uma melhor
      legibilidade e consistência.
    - Removida a mensagem de erro "ErrFindByIdUsuario" que não era usada.
    - Adicionada nova mensagem de erro "ErrUsuarioUnauthorized" para indicar
      quando um usuário não está autorizado.
    - Ajustada a formatação e indentação do código.
    
    Issue: #123

--- internal/application/repositories/errors/usuarios.go
+++ internal/application/repositories/errors/usuarios.go
@@ -1,11 +1,8 @@
 package errors
 
 const (
-	ErrFindByIdUsuario      = "não foi possível encontrar a usuario"
-	ErrUsuarioNotFound      = "usuario não encontrada"
-	ErrUsuarioIdIsNull      = "o id da usuario não pode ser nulo"
-	ErrUsuarioEmailIsNull   = "o email da usuario não pode ser nulo"
-	ErrUpdateUsuario        = "não foi possível atualizar a usuario"
-	ErrDeleteUsuario        = "não foi possível deletar a usuario"
-	ErrUsuarioAlreadyExists = "usuario já existe"
+	ErrUsuarioNotFound     = "usuario não encontrada"
+	ErrUsuarioIdIsNull     = "o id da usuario não pode ser nulo"
+	ErrUsuarioEmailIsNull  = "o email da usuario não pode ser nulo"
+	ErrUsuarioUnauthorized = "usuario não autorizado"
 )
