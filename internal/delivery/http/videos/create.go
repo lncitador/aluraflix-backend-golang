339b74678b9f47e049e2f4dcb30bbd0fe6647901
Walaff Fernandes <lncitador@icloud.com>
2023-09-29 15:15:32 -0300

    feat(http): adiciona endpoint para criação de vídeos
    Adiciona endpoint para criação de vídeos, onde o usuário é obtido do contexto e atribuído ao DTO de entrada. Em seguida, o vídeo é criado utilizando o caso de uso correspondente.

--- internal/delivery/http/videos/create.go
+++ internal/delivery/http/videos/create.go
@@ -6,11 +6,14 @@ import (
 )
 
 func (h VideoHandlers) create(c *gin.Context) {
+	user, _ := c.Get("userId")
 	var dto domain.VideoInput
 	if err := c.ShouldBindJSON(&dto); err != nil {
 		c.JSON(400, gin.H{"error": err})
 		return
 	}
+
+	dto.UsuarioID = &user.(*domain.UsuarioDto).ID
 
 	video, err := h.useCase.Create(dto)
 	if err != nil {
